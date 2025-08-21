480de8c7bc4a3ea212057b2c82f4791bf4d4bf91
Isabela Rodrigues de Souza <bela.rodjj@gmail.com>
2025-08-21 17:01:59 -0300

    header and footer created!

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./template/template-module').then((m) => m.TemplateModule), //carregando todas as rotas (lazy-load)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
